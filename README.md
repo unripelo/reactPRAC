397c6b09e13a6e159113513503286ba3c52230ae
Ira Narisma <narisma.ira.chloie@gmail.com>
2025-01-25 18:08:35 +0800

    adding mobile res

--- README.md
+++
@@ -1,3 +0,0 @@
-# my-first project
-
-my-first-project is a library for practicing on how to make an UI
--- react-dev/src/App.tsx
+++ react-dev/src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="flex h-screen w-screen items-center justify-center align-middle">
-        <div className="flex bg-red-800 items-center justify-center font-bold text-2xl h-2/3 aspect-square rounded-3xl flex-col p-10 space-y-10">
+        <div className="flex bg-red-800 items-center w-full justify-center font-bold text-2xl h-full md:h-2/3 md:max-w-screen-md  md:aspect-square rounded-3xl flex-col p-10 space-y-10">
           <div className="bg-white flex size-full items-center justify-center p-4 rounded-lg">
             <div
               className="
@@ -20,7 +17,7 @@ function App() {
               Drawer 1
             </div>
           </div>
-          <div className="bg-white flex size-full items-center justify-center p-4 space-x-5 rounded-lg">
+          <div className="bg-white flex size-full flex-col md:flex-row items-center justify-center p-4 space-y-2 md:space-y-0 md:space-x-5 rounded-lg">
             <div
               className="bg-red-800 text-white size-full items-center
               justify-center flex flex-1 hover:flex-[2]"
@@ -34,7 +31,7 @@ function App() {
               Drawer 3
             </div>
           </div>
-          <div className="bg-white flex size-full items-center justify-center p-4 space-x-4 rounded-lg">
+          <div className="bg-white flex size-full items-center justify-center p-4  space-y-2 md:space-y-0 md:space-x-4 rounded-lg flex-col md:flex-row">
             <div
               className="bg-red-800 text-white size-full items-center
               justify-center flex flex-1 hover:flex-[2]"
