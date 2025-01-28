55f8a02a99094deddbe68eb763bfcaf0f809122a
Ira Narisma <narisma.ira.chloie@gmail.com>
2025-01-28 22:31:32 +0800

    editing 1 and 2

--- react-dev/src/Content1Container.tsx
+++ react-dev/src/Content1Container.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Content1Container = () => {
   return (
-    <div className="w-2/3 border border-black-400 h-[80%] rounded-xl p-5 flex items-center justify-center">
-      <div className="text-2xl font-semibold">Hello, 1!</div>
+    <div className="flex h-screen w-screen items-center justify-center align-middle">
+      <div className="w-2/3 border-4 border-black-400  rounded-xl flex-col p-10 space-y-10 flex items-center justify-center align-middle">
+        <div className="text-2xl font-semibold">Hello, 1!</div>
+      </div>
     </div>
   );
 };
--- react-dev/src/Content2Container.tsx
+++ react-dev/src/Content2Container.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Content2Container = () => {
   return (
-    <div className="w-2/3 border border-black-100 rounded-xl p-5 flex items-center justify-center">
-      <div className="text-2xl font-semibold">Hello, 2!</div>
+    <div className="flex h-screen w-screen items-center justify-center align-middle">
+      <div className="w-2/3 border-4 border-black-400  rounded-xl flex-col p-10 space-y-10 flex items-center justify-center align-middle">
+        <div className="text-2xl font-semibold">Hello, 2!</div>
+      </div>
     </div>
   );
 };
