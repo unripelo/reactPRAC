5fc4d835922b237a073ffe8212c68897d69aaa1a
Ira Narisma <narisma.ira.chloie@gmail.com>
2025-01-30 22:18:05 +0800

    adding text

--- react-dev/src/Content1Container.tsx
+++ react-dev/src/Content1Container.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Content1Container = () => {
+  const text = "Add story here";
   return (
     <div className="flex h-screen w-screen items-center justify-center align-middle">
       <div className="w-2/3 h-5/6 border-4 border-black-400 bg-amber-100 rounded-xl flex-col p-10 space-y-10 flex items-center justify-center align-middle">
@@ -8,7 +9,7 @@ const Content1Container = () => {
           className="bg-white border-4 rounded-lg
         flex size-90 items-center w-full text-xl overflow-y-scroll"
         >
-          <p className="font-serif text-left px-8">{}</p>
+          <p className="font-serif text-left px-8">{text}</p>
         </div>
         <div className="bg-white p-4 rounded-lg absolute bottom-16 left-65 text-2xl border-4 w-45 items-center justify-center flex size-15">
           Heart
