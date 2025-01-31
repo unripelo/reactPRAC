1eaea447800adff5e751d6caf9ad72ed3036f2c3
Ira Narisma <narisma.ira.chloie@gmail.com>
2025-01-31 08:47:54 +0800

    adding div

--- react-dev/src/Content2Container.tsx
+++ react-dev/src/Content2Container.tsx
@@ -4,7 +4,8 @@ const Content2Container = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center align-middle">
       <div className="w-2/3 h-5/6 border-4 border-black-400 bg-amber-100 rounded-xl flex-col p-10 space-y-10 flex items-center justify-center align-middle">
-        <div className="text-2xl font-semibold">2048 Model</div>
+        <div className="text-5xl font-bold">2048</div>
+        <div className="text-2xl font-semibold">Endless Mode</div>
       </div>
     </div>
   );
