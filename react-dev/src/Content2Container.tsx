5100ceb69d92a3507f18e3593cf5b2839154ff73
Ira Narisma <narisma.ira.chloie@gmail.com>
2025-01-31 08:41:25 +0800

    adding content 2

--- react-dev/src/Content2Container.tsx
+++ react-dev/src/Content2Container.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const Content2Container = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center align-middle">
-      <div className="w-2/3 border-4 border-black-400  rounded-xl flex-col p-10 space-y-10 flex items-center justify-center align-middle">
-        <div className="text-2xl font-semibold">Hello, 2!</div>
+      <div className="w-2/3 h-5/6 border-4 border-black-400 bg-amber-100 rounded-xl flex-col p-10 space-y-10 flex items-center justify-center align-middle">
+        <div className="text-2xl font-semibold">2048 Model</div>
       </div>
     </div>
   );
