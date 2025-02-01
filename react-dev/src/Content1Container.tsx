48e89f9ace14fdab292b00965800ff08ad2dfd40
Ira Narisma <narisma.ira.chloie@gmail.com>
2025-02-01 23:35:58 +0800

    adding color

--- react-dev/src/Content1Container.tsx
+++ react-dev/src/Content1Container.tsx
@@ -4,7 +4,7 @@ const Content1Container = () => {
   const text = "Add story here";
   return (
     <div className="flex h-screen w-screen items-center justify-center align-middle">
-      <div className="w-2/3 h-5/6 border-4 border-black-400 bg-amber-100 rounded-xl flex-col p-10 space-y-10 flex items-center justify-center align-middle">
+      <div className="w-2/3 h-5/6 border-4 border-black-400 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-xl flex-col p-10 space-y-10 flex items-center justify-center align-middle">
         <div className="bg-white p-4 rounded-lg border-4 text-2xl absolute top-40 left-60">
           Add Chapter Name
         </div>
