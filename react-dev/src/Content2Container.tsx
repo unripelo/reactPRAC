40d2c6a42599c12f2f8df29ce470a1e302665dd7
Ira Narisma <narisma.ira.chloie@gmail.com>
2025-02-01 23:25:24 +0800

    adding border

--- react-dev/src/Content2Container.tsx
+++ react-dev/src/Content2Container.tsx
@@ -4,8 +4,10 @@ const Content2Container = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center align-middle">
       <div className="w-2/3 h-5/6 border-4 border-black-400 bg-amber-100 rounded-xl flex-col p-10 space-y-10 flex items-center justify-center align-middle">
-        <div className="text-5xl font-bold">2048</div>
-        <div className="text-2xl font-semibold">Endless Mode</div>
+        <div className="p-4 rounded-lg border-4">
+          <div className="text-5xl font-bold">2048</div>
+          <div className="text-2xl font-semibold">Endless Mode</div>
+        </div>
       </div>
     </div>
   );
