87fa32fc688703111a84a36820511b2029f90a3a
Ira Narisma <narisma.ira.chloie@gmail.com>
2025-02-03 23:06:06 +0800

    replacing smth

--- react-dev/src/App.tsx
+++ react-dev/src/App.tsx
@@ -7,57 +7,6 @@ function App() {
       <div>
         <MainContainer />
       </div>
-      <div className="flex h-screen w-screen items-center justify-center align-middle">
-        <div className="flex bg-red-800 items-center w-full justify-center font-bold text-2xl h-full md:h-2/3 md:max-w-screen-md  md:aspect-square rounded-3xl flex-col p-10 space-y-10">
-          <div className="bg-white flex size-full items-center justify-center p-4 rounded-lg">
-            <div
-              className="
-              bg-red-800
-              flex
-              size-full
-              items-center
-              justify-center text-white"
-            >
-              Ada Lovelace
-            </div>
-          </div>
-          <div className="bg-white flex size-full flex-col md:flex-row items-center justify-center p-4 space-y-2 md:space-y-0 md:space-x-5 rounded-lg">
-            <div
-              className="bg-red-800 text-white size-full items-center
-              justify-center flex flex-1 hover:flex-[2] transition-flex duration-300 ease-in-out"
-            >
-              Alan Turing
-            </div>
-            <div
-              className="bg-red-800 text-white size-full items-center
-              justify-center flex flex-1 hover:flex-[2] transition-flex duration-300 ease-in-out "
-            >
-              Guido Van Rossum
-            </div>
-          </div>
-          <div className="bg-white flex size-full items-center justify-center p-4  space-y-2 md:space-y-0 md:space-x-4 rounded-lg flex-col md:flex-row">
-            <div
-              className="bg-red-800 text-white size-full items-center
-              justify-center flex flex-1 hover:flex-[2]
-              transition-flex duration-300 ease-in-out"
-            >
-              Grace Hopper
-            </div>
-            <div
-              className="bg-red-800 text-white size-full items-center
-              justify-center flex flex-1 hover:flex-[2] transition-flex duration-300 ease-in-out"
-            >
-              JV Neumann
-            </div>
-            <div
-              className="bg-red-800 text-white size-full items-center
-              justify-center flex flex-1 hover:flex-[2] transition-flex duration-300 ease-in-out"
-            >
-              Junar Landicho
-            </div>
-          </div>
-        </div>
-      </div>
     </>
   );
 }
--- react-dev/src/Content2Container.tsx
+++ react-dev/src/Content2Container.tsx
@@ -3,10 +3,9 @@ import React from "react";
 const Content2Container = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center align-middle">
-      <div className="w-2/3 h-5/6 border-4 border-black-400 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-xl flex-col p-10 space-y-10 flex items-center justify-center align-middle">
-        <div className="p-4 rounded-lg border-4">
-          <div className="text-5xl font-bold">2048</div>
-          <div className="text-2xl font-semibold">Endless Mode</div>
+      <div className="border-0 border-black-400 bg-black rounded-xl flex-col flex items-center justify-center align-middle">
+        <div className="p-4 rounded-lg border-4 absolute bottom-1.5 right-40 bg-black text-white">
+          <div className="text-xl font-semibold">Thank you for reading!</div>
         </div>
       </div>
     </div>
