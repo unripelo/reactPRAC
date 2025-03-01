c0b7bafc0c222e5c9ffe08d052732c5eeeff19ff
Ira Narisma <narisma.ira.chloie@gmail.com>
2025-03-01 23:25:10 +0800

    editing file

--- react-dev/src/Content1Container.tsx
+++ react-dev/src/Content1Container.tsx
@@ -4,14 +4,14 @@ const Content1Container = () => {
   const text = "Add story here";
   const text1 = "Chapter 1";
   return (
-    <div className="flex h-screen w-screen items-center justify-center align-middle">
-      <div className="w-2/3 h-5/6 border-4 border-black-400 bg-white rounded-xl flex-col p-10 space-y-10 flex items-center justify-center align-middle">
-        <div className="bg-white p-4 rounded-lg border-4 text-2xl absolute top-40 left-60 font-serif flex md:h-18 md:max-w-screen-md">
+    <div className="flex w-md h-screen items-center justify-center align-middle">
+      <div className="w-2/3 h-5/6 border-4 border-black-400 bg-amber-400 rounded-xl flex-col p-10 space-y-10 flex items-center justify-center align-middle">
+        <div className="bg-red-300 p-4 rounded-lg border-4 text-2xl absolute top-40 left-60 font-serif flex md:h-18 md:max-w-screen-md">
           {text1}
         </div>
         <div
           className="bg-white border-4 rounded-lg
-        flex size-90 items-center w-full text-xl overflow-y-scroll md:shrink-0 md:h-2/3 md:max-w-screen-md"
+        flex size-40 items-center w-full text-xl overflow-y-scroll md:shrink-0 md:h-2/3 md:max-w-screen-md"
         >
           <p className="font-serif text-left px-8 text-2xl flex">{text}</p>
         </div>
@@ -27,7 +27,7 @@ const Content1Container = () => {
             </svg>
           </div>
         </div>
-        <div className="bg-white p-4 rounded-4xl absolute bottom-27 right-60 text-2xl border-4 w-70 items-center justify-center flex size-15  md:shrink md:h-15 md:max-w-screen-md">
+        <div className="bg-white p-4 rounded-4xl absolute bottom-27 right-60 text-2xl border-4 w-70 items-center justify-center flex size-15  md:shrink md:h-15 md:max-w-screen-md font-semibold">
           Comment
         </div>
       </div>
